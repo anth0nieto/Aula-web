15e148751d962d521af249df66b0a262bc1c25b0
anth0 <antho.montero>
2018-04-28 04:08:28 +0000

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB5npiNawgvftP5CWbhYXPB-o7z5yjHncs',
+    authDomain: 'aula-web-20aac.firebaseapp.com',
+    databaseURL: 'https://aula-web-20aac.firebaseio.com',
+    projectId: 'aula-web-20aac',
+    storageBucket: 'aula-web-20aac.appspot.com',
+    messagingSenderId: '181265769919'
+  }
 };
